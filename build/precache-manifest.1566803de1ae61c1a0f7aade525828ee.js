bac583592faf85e5cd1af6e975537b6af80120ad
Azhar <aazhar007@gmail.com>
2020-05-08 16:35:49 +0530

    missed checkin

---
+++ build/precache-manifest.1566803de1ae61c1a0f7aade525828ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf9b088d31a19fcc3315a7fa7fe0782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f425fdf1fcc38257167",
+    "url": "/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "4a463fe46940b421144b",
+    "url": "/static/css/main.dde42172.chunk.css"
+  },
+  {
+    "revision": "4f425fdf1fcc38257167",
+    "url": "/static/js/2.035285ce.chunk.js"
+  },
+  {
+    "revision": "a413d91d31ffdd936abfdb03bdd22ff5",
+    "url": "/static/js/2.035285ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a463fe46940b421144b",
+    "url": "/static/js/main.a6ac63bb.chunk.js"
+  },
+  {
+    "revision": "3c180fd17772e3064e9b",
+    "url": "/static/js/runtime-main.8ec97f09.js"
+  }
+]);
