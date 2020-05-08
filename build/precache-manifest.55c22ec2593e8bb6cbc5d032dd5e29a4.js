2c2bd660b3232cc94915e932c37d0e5b518763ba
Azhar <aazhar007@gmail.com>
2020-05-08 15:07:39 +0530

    Added react build

---
+++ build/precache-manifest.55c22ec2593e8bb6cbc5d032dd5e29a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b50c51d552febc355a535238bf27b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f425fdf1fcc38257167",
+    "url": "/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "cddd81ac078c9abe945d",
+    "url": "/static/css/main.dde42172.chunk.css"
+  },
+  {
+    "revision": "4f425fdf1fcc38257167",
+    "url": "/static/js/2.035285ce.chunk.js"
+  },
+  {
+    "revision": "a413d91d31ffdd936abfdb03bdd22ff5",
+    "url": "/static/js/2.035285ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cddd81ac078c9abe945d",
+    "url": "/static/js/main.5881bec5.chunk.js"
+  },
+  {
+    "revision": "3c180fd17772e3064e9b",
+    "url": "/static/js/runtime-main.8ec97f09.js"
+  }
+]);
---
+++ routes/index.js
@@ -0,0 +1,81 @@
+﻿const CONSTANTS = require("../constants");
+const express = require("express");
+const sampleData = require("../sampleMeterData");
+
+
+const router = express.Router();
+// LIST ENDPOINTS
+router.get(CONSTANTS.ENDPOINT.LIST, function (req, res) {
+  res.json(sampleData.listTextAssets);
+});
+
+router.get(CONSTANTS.ENDPOINT.METERSERIALNUMBERS, async function (req, res) {
+  sampleData.getMeterData().then((result) => {
+    // console.log(result);
+    result = result.map(({ Serial }) => Serial)
+    result = [... new Set(result)]
+    res.json(result);
+  });
+
+});
+
+router.get(CONSTANTS.ENDPOINT.METERSERIALNUMBERS, async function (req, res) {
+  sampleData.getMeterData().then((result) => {
+    // console.log(result);
+    result = result.map(({ Serial }) => Serial)
+    result = [... new Set(result)]
+    res.json(result);
+  });
+
+});
+
+router.get(CONSTANTS.ENDPOINT.METERDETAILS + "/:_id", async function (req, res) {
+  const { _id } = req.params;
+  sampleData.getMeterData().then((result) => {
+    // console.log(result);
+    result.sort(function (a, b) {
+      var dateA = new Date(a.ReadingDateTimeUTC), dateB = new Date(b.ReadingDateTimeUTC);
+      return dateA - dateB;
+    });
+    result = result.filter(x => x.Serial == _id);
+    result = [...new Set(result)]
+    // console.log("result::", result);
+
+    const xaxisresult = result.map(({ ReadingDateTimeUTC }) => ReadingDateTimeUTC)
+    // console.log("xaxisresult", xaxisresult);
+
+    const yaxis1result = result.map(({ WH }) => Number(WH))
+    // console.log("yaxis1result::", yaxis1result);
+
+    const y1 = diff(yaxis1result);
+
+    // xaxisresult.pop();
+    // xaxisresult.pop();
+    xaxisresult.shift();
+    //  y1.shift();
+    // y1.pop();
+    // console.log("Y1", y1.length, y1[y1.length], y1[y1.length - 1], y1[y1.length - 2], y1[0], "xaxisresult", xaxisresult.length);
+
+    const yaxis2result = result.map(({ VARH }) => Number(VARH))
+    const y2 = diff(yaxis2result);
+    const obj = {
+      xaxis: xaxisresult,
+      yaxis1: y1,
+      yaxis2: y2
+    }
+    console.log("Final return Object::", obj);
+
+    res.json(obj);
+  });
+
+});
+
+
+function diff(ary) {
+  var newA = [];
+  for (var i = 1; i < ary.length; i++)  newA.push(ary[i] - ary[i - 1])
+  return newA;
+}
+
+
+module.exports = router;
