a67a20c3a26964975c50a701356db8c113dd02be
Azhar <aazhar007@gmail.com>
2020-05-14 10:00:04 +0530

    New build lazy loading change.

--- DevelopmentEnvirnment/src/App.jsx
+++ DevelopmentEnvirnment/src/App.jsx
@@ -1,25 +1,30 @@
-﻿import React from "react";
+﻿import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 // import Blank from "./components/Blank/Blank";
+// const Home = lazy(() => import('./routes/Home'));
+const Meter = lazy(() => import('./components/Meter/Meter'));
 
-import Meter from "./components/Meter/Meter";
+
+// import Meter from "./components/Meter/Meter";
 
 //TODO Web Template Studio: Add routes for your new pages here.
 const App = () => {
-    return (
-      <React.Fragment>
-        <NavBar />
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route exact path = "/" component = { Meter } />
-          <Route path = "/Meter" component = { Meter } />
+          <Route exact path="/" component={Meter} />
+          <Route path="/Meter" component={Meter} />
         </Switch>
-        <Footer />
-      </React.Fragment>
-    );
+      </Suspense>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ build/precache-manifest.d51c7c00a74e19083d7d1931a52b935e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1eea28ae462db6e985c879ee097e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2346c6042c0c2646d481",
+    "url": "/static/css/3.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "5ab834c3b5c7614e34d7",
+    "url": "/static/css/4.d948069f.chunk.css"
+  },
+  {
+    "revision": "11d418d022b1c2cc2616",
+    "url": "/static/css/main.823c6f45.chunk.css"
+  },
+  {
+    "revision": "b4aa4ed8fd9ba683255a",
+    "url": "/static/js/2.cd361b6c.chunk.js"
+  },
+  {
+    "revision": "3185cd37a95147bce68533a5d11c45c0",
+    "url": "/static/js/2.cd361b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2346c6042c0c2646d481",
+    "url": "/static/js/3.4582de0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.4582de0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab834c3b5c7614e34d7",
+    "url": "/static/js/4.9fec176c.chunk.js"
+  },
+  {
+    "revision": "11d418d022b1c2cc2616",
+    "url": "/static/js/main.ad85d362.chunk.js"
+  },
+  {
+    "revision": "f47e1d9e039456540565",
+    "url": "/static/js/runtime-main.ad1fbade.js"
+  }
+]);
